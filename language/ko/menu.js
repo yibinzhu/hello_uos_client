008596fa923ca28b5158d11393e528c078a3ffea
yibinzhu <for.east@163.com>
2017-02-21 17:25:03 +0900

    底部导航
    加入底部导航条

---
+++ language/ko/menu.js
@@ -0,0 +1,15 @@
+var lang={
+"title":"Hello UOS",
+"major":"정공시간표",
+"minor":"교양시간표",
+"map":"학교맵",
+"notice":"학교공지",
+"schedule":"학교일정",
+"tuition":"등록금내부시간표",
+"lab":"도서관",
+"fourm":"학생포름",
+"club":"학생클업",
+"restaurant":"식당메뉴",
+"professor":"교수갬색",
+"addresses":"교내연략저"
+};
